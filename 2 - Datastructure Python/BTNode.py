f6465ea3aca6f76d0b4499c7f9303ba23f5c477e
jugshaurya <shauryasinghal84@gmail.com>
2019-03-10 13:27:21 +0530

    more function added to BT

--- 2 - Datastructure Python/BTNode.py
+++ 2 - Datastructure Python/BTNode.py
@@ -1,4 +1,5 @@
 class BinaryTreeNode():
+	
 	def __init__(self, data):
 		self.data = data
 		self.left = None
--- 2 - Datastructure Python/BinaryTree.py
+++ 2 - Datastructure Python/BinaryTree.py
@@ -6,14 +6,30 @@ def printTree_preorder(root):
 
     print(root.data, ' : ', end = '')
     if root.left != None:
-        print(root.left.data, end=',')
+        print(root.left.data, end = ',')
     if root.right != None:
-        print(root.right.data, end=' ')
+        print(root.right.data, end = ' ')
     print()
     
     printTree_preorder(root.left)
     printTree_preorder(root.right)
 
+def printTree_postorder(root):
+    if root == None:
+        return
+    
+    printTree_postorder(root.left)
+    printTree_postorder(root.right)
+    print(root.data)
+
+def printTree_inorder(root):
+    if root == None:
+        return
+    
+    printTree_postorder(root.left)
+    print(root.data)
+    printTree_postorder(root.right)
+    
 def buildTree():
     # li = [int(x) for x in input().split()] # given input in a line only, data is seperated by space
     root = None
@@ -31,5 +47,96 @@ def buildTree():
     root = node1
     return root
 
-root = buildTree()
+def userBuiltTree():
+    rootData = int(input())
+
+    # -1 is used to stop continuing over the current branch - so taking input in depth-first Style
+    if rootData == -1:
+        return None
+
+    root = BinaryTreeNode(rootData)
+    root.left = userBuiltTree()
+    root.right = userBuiltTree()    
+    return root
+
+def nodesInTree(root):
+    
+    if root is None:
+        return 0
+    leftCount = nodesInTree(root.left)
+    rightCount = nodesInTree(root.right)
+    return 1 + leftCount + rightCount
+
+def largest_node(root):
+    if root==None:
+        return -2**31 # returing -inf
+    
+    leftMax = largest_node(root.left)
+    rightMax = largest_node(root.right)
+    rootData = root.data
+    return max(rootData, leftMax,rightMax)
+
+def nodes_gt_x(root,x):
+    if root==None:
+        return 
+    
+    nodes_gt_x(root.left,x)
+    nodes_gt_x(root.right,x)
+    if root.data > x:
+        print(root.data)
+
+def sumOfAllNodes(root):
+    if root==None :
+        return 0
+    leftSum = sumOfAllNodes(root.left)
+    rightSum = sumOfAllNodes(root.right)
+    return root.data  + leftSum + rightSum
+
+def treeHeight(root):        
+
+    if root is None:
+        return 0
+
+    leftHeight = treeHeight(root.left)
+    rightHeight = treeHeight(root.right)
+
+    return 1+ max(leftHeight, rightHeight)
+
+def numberOfLeaveNodes(root):
+    if root is None:
+        return 0
+    
+    leftans = numberOfLeaveNodes(root.left)
+    rightans = numberOfLeaveNodes(root.right)
+    result = leftans + rightans
+    if result == 0 :
+        result+=1
+    return result
+
+print('Enter the data of tree : ')
+# Given input:  1 2 4 -1 -1 5 -1 -1 3 -1 7 -1 -1 
+root = userBuiltTree()
+print('Preorder Print : ')
 printTree_preorder(root)
+
+print('Numbers of Nodes are : ', nodesInTree(root))
+
+print('Postorder Print : ')
+printTree_postorder(root)
+
+print('Inorder Print : ')
+printTree_inorder(root)
+
+print('Max Node Data is : ', largest_node(root) )
+print('Sum of All nodes is : ', sumOfAllNodes(root))
+print('Height of tree is : ', treeHeight(root))
+print('Number of leaves nodes are : ', numberOfLeaveNodes(root))
+
+x=int(input('Enter x '))
+print('Values greater than %d are : '%x)
+nodes_gt_x(root,x)
+
+
+
+
+
