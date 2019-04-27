aab1bf2c68cbd231270b4daa16281592f3d2a8ca
jugshaurya <shauryasinghal84@gmail.com>
2019-04-27 17:48:22 +0530

    tree code added

--- 2 - Programs including Datastructure Python/Binary Tree/diameter_of_tree.py
+++ 2 - Programs including Datastructure Python/Binary Tree/diameter_of_tree.py
@@ -1,16 +1,11 @@
 import queue
-
 class BinaryTreeNode():
-
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-
-
 def height(root):
-
     if root == None:
         return 0 
 
@@ -19,33 +14,29 @@ def height(root):
 
     return 1 + max(lh,rh)
 
-# if height difference of all left and right subtree is -1,0,1, then tree is balanced.
-def isBalanced(root):
+def diameter_tree(root):
 
     if root == None:
-        return True
+        return 0
 
+    ld = diameter_tree(root.left)
+    rd = diameter_tree(root.right)
 
-    isLeftBalanced = isBalanced(root.left)
-    isRightBalanced = isBalanced(root.right)
     lh = height(root.left)
     rh = height(root.right)
+    
+    return max(ld, rd, 1 + lh + rh)
 
-    return isLeftBalanced and isRightBalanced and (lh-rh) in [-1,0,1]
+def diameter_tree_improved(root):
 
-def isBalanced_improved_helper(root):
+    if root == None:
+        return (0,0)
 
-    if root == None :
-        return (True, 0)
-
-    isLeftBalanced, lh = isBalanced_improved_helper(root.left)
-    isRightBalanced, rh = isBalanced_improved_helper(root.right)
-
-    return (isLeftBalanced and isRightBalanced and (lh-rh) in [-1,0,1], 1 + max(lh,rh) )
-
-def isBalanced_improved(root):
-    isBalancedans , height = isBalanced_improved_helper(root)
-    return isBalancedans 
+    ld,lh = diameter_tree_improved(root.left)
+    rd,rh = diameter_tree_improved(root.right)
+    
+    total_h = 1 + max(lh , rh)
+    return max(ld, rd, lh + rh + 1), total_h 
 
 def buildLevelTree(levelorder):
     index = 0
@@ -92,21 +83,14 @@ def printLevelATNewLine(root):
         inputQ, outputQ = outputQ, inputQ
 
 # Main
-# 1 2 -1 3 -1 -1 -1 -> not balanced
-# 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1 -> balanced 
 levelOrder = [int(i) for i in input().split()]
 root = buildLevelTree(levelOrder)
 printLevelATNewLine(root)
+print()
 
-ans = isBalanced(root)
-if ans :
-    print('Tree is Balanced.')
-else:
-    print('Tree is not Balanced.')
+d = diameter_tree(root)
+print('Diameter of tree is : ', d)
 
+d = diameter_tree_improved(root)[0]
+print('Diameter of tree is : ', d)
 
-ans = isBalanced_improved(root)
-if ans :
-    print('Tree is Balanced.')
-else:
-    print('Tree is not Balanced.')
